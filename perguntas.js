cd93a5b8e3357ca84b8540ff8f4cd8df74d77296
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-09-18 23:40:10 -0300

    r

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Informática',
+    'Qual o melhor canal para aprender informática',
+    '@rasinformática'
+)
+
+criaCartao(
+    'Qual é a melhor turma do CERB',
+    'A que mais se dedica as aulas do Prof. Rafael?',
+    'Estou em dúvida'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos titulo mundial o palmeiras têm?',
+    'Nenhum 🤣🤣🤣'
+)
